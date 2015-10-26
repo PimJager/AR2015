9bdf59cad559890519716d129f7b4159fe5d0ec0
pimjager <pim@pimjager.nl>
2015-10-26 21:22:10 +0100

    Stated 2

---
+++ AS1/two.hs
@@ -0,0 +1,104 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import qualified Data.Text as T
+import Control.Monad.Writer
+import Data.Monoid
+import Data.Char
+import Data.List
+
+type YicesWriter = Writer T.Text ()
+
+tellLn :: T.Text -> Writer T.Text ()
+tellLn w = tell (w `T.snoc` '\n')
+
+showT :: Show a => a -> T.Text
+showT a = T.pack $ show a
+
+(<<<) = T.append
+
+data Component = C { ci:: Int, cw :: Int, ch :: Int } 
+    deriving (Eq, Show)
+
+powerC      = map (\i-> C i 2 4) [1..3]
+regularC    = zipWith (\i (w,h) -> C i w h) [4..] [(9,7), (12,6), (10,7), (18,5), (20,4), (10,6), (8,6), (10,8)] 
+dimW        = 29
+dimH        = 22
+
+x c = "x_" <<< (showT $ ci c)
+y c = "y_" <<< (showT $ ci c)
+w c = "w_" <<< (showT $ ci c)
+h c = "h_" <<< (showT $ ci c)
+
+variables :: [Component] -> YicesWriter
+variables cs = do
+    tellLn ";; Define all vars"
+    mapM_ components cs
+    tellLn ""
+    where
+        components c = mapM_ (\f-> defV f c) [x,y,w,h]
+        defV f c = tellLn $ "(define " <<< (f c) <<< "::int)"
+
+dimensions :: [Component] -> YicesWriter
+dimensions cs = do
+    tellLn "\t;; Components dimensions"      
+    mapM_ components cs
+    tellLn ""
+    where
+        components c = do 
+            tellLn  $ "\t (= " <<< w c <<< " " <<< (showT . cw) c <<< ")"
+            tellLn  $ "\t (= " <<< h c <<< " " <<< (showT . ch) c <<< ")"
+
+
+inside :: [Component] -> YicesWriter
+inside cs = do 
+    tellLn "\t;; Components should fit in the chip"
+    mapM_ components cs
+    tellLn ""
+    where
+        components c = do
+            tell    $ "\t (<= 0 " <<< x c <<< ") "
+            tellLn  $ "(<= " <<< x c <<< " (- " <<< showT dimW <<< " " <<< w c <<< "))"
+            tell    $ "\t (<= 0 " <<< y c <<< ") "
+            tellLn  $ "(<= " <<< y c <<< " (- " <<< showT dimH <<< " " <<< h c <<< "))"
+
+overlap :: [Component] -> YicesWriter
+overlap cs = do
+    tellLn "\t;; components should not overlap"
+    mapM_ components [(c1,c2) | c1 <- cs, c2<-(delete c1 cs)]
+    tellLn ""
+    where
+        components (c1,c2) = do
+            tellLn  $ "\t (not (and "
+            tellLn  $ "\t\t (<= " <<< x c2 <<< " " <<< x c1 <<< ")"
+            tellLn  $ "\t\t (< " <<< x c1 <<< " (+ " <<< x c2 <<< " " <<< w c2 <<< "))"
+            tellLn  $ "\t\t (<= " <<< y c2 <<< " " <<< y c1 <<< ")"
+            tellLn  $ "\t\t (< " <<< y c1 <<< " (+ " <<< y c2 <<< " " <<< h c2 <<< "))"
+            tellLn  $ "\t))"
+
+heat :: [Component] -> YicesWriter
+heat cs = do
+    tellLn "\t;; Due to heat power components can not be to close"
+    mapM_ components [(c1,c2) | c1 <- cs, c2<-(delete c1 cs)]
+    tellLn ""
+    where
+        components (c1,c2) = do
+             
+
+
+main = putStr $ T.unpack $ execWriter $ (variables (powerC++regularC) >> env)
+    where
+        env = do
+            tellLn "(assert (or"
+            mapM_ writers permuations
+            tellLn "))"
+            tellLn "(check)"
+            tellLn "(show-model)"
+        writers (pc, rc) = do -- all writers expect to be executed within the aseert - or block
+            tellLn "  (and"
+            dimensions (pc++rc)
+            inside (pc++rc)
+            overlap (pc++rc)
+            tellLn "  )"
+        permuations = [(powerC, regularC)] -- todo: make permutations
